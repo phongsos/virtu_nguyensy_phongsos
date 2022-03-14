3a6523eebc0c329ab89cb321b98997b49f1e200e
phongsos <saysana.phongsouvanh@edu.esiee.fr>
2022-03-14 19:45:40 +0100

    Add microserv3

---
+++ microserv3/src/main/java/esiee/ngupho/microserv3/MicroServ3Application.java
@@ -0,0 +1,13 @@
+package esiee.ngupho.microserv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServ3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServ3Application.class, args);
+    }
+
+}
---
+++ microserv3/src/test/java/esiee/ngupho/microserv3/MicroServ3Tests.java
@@ -0,0 +1,13 @@
+package esiee.ngupho.microserv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServ3Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
