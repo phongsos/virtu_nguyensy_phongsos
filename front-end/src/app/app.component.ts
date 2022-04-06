9cb11a4db0afce295d683c563e7581ff65667c8f
phongsos <saysana.phongsouvanh@edu.esiee.fr>
2022-04-07 00:27:12 +0200

    IT WORKS

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -10,12 +10,13 @@ export class AppComponent {
   title = 'front-end';
   imageSearch: string = "";
   url_backend = 'http://localhost:31001';
+  outputs: Array<String> = [];
 
   constructor(private http: HttpClient) { }
 
   searchImage() {
     this.http.get(this.url_backend + "/current?location=" + this.imageSearch, {responseType: 'text'}).subscribe(data => {
-      console.log(data);
+      this.outputs.push(data);
     })
   }
 }
