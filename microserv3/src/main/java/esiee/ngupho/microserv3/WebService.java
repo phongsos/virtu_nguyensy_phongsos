3f5470f2990faeee6067ccd1fb091ec201348af1
Sylvie Nguyen <sylvie.nguyen@edu.esiee.fr>
2022-04-07 14:19:00 +0200

    Fix CORS

--- microserv3/src/main/java/esiee/ngupho/microserv3/WebService.java
+++ microserv3/src/main/java/esiee/ngupho/microserv3/WebService.java
@@ -8,15 +8,14 @@ import esiee.ngupho.microserv2.dto.PhotoDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 
+
 @RestController
+@CrossOrigin
 public class WebService {
 
     @Value("${microserv1URL}")
